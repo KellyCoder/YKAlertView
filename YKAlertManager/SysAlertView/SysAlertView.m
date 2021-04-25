5ef2a24528203829b418891a90e580cb02999aca
杨凯 <15185566062@163.com>
2021-04-25 21:53:29 +0800

    UIAlertController封装

---
+++ YKAlertManager/SysAlertView/SysAlertView.m
@@ -0,0 +1,20 @@
+//
+//  SysAlertView.m
+//  YKAlertViewDemo
+//
+//  Created by Kevin on 2021/4/25.
+//
+
+#import "SysAlertView.h"
+
+@implementation SysAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
